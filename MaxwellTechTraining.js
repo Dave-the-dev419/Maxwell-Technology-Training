3db786ca400eba72301e64c6e64e3af3212b9c27
Dave Ndecham <109639325+Dave-the-dev419@users.noreply.github.com>
2023-07-11 19:51:57 +0100

    Update MaxwellTechTraining.js

--- MaxwellTechTraining.js
+++ MaxwellTechTraining.js
@@ -1,44 +1,73 @@
-var Questions = {
-    questionOne: {
+var questions = [
+    {
         question: "What are your Career Objectives?",
         response: ["International Certification", "Job", "Expand my Knowledge"]
     },
-    questionTwo: {
+    {
         question: "Are you currently employed?",
         response: ["Yes", "No"]
     },
-    questionThree: {
+   {
         question: "Do you have any short-term travel plans?",
         response: ["yes", "no"]
     },
-    questionFour: {
-        question: "What is your curernt level of formal education?",
-        response: ["O' level", "A' level", "Associate degree", "Undergraduate degree", "Postgraduate degree"]
+    {
+        question: "How long will your travel last?",
+        response: ["0-3 months", "3-12 months", "Over a year"]
     },
-    questionFive: {
+    {
+        question: "What is your curernt level of formal education?",
+        response: ["O' level", "A' level", "Undergraduate degree", "Postgraduate degree"]
+    },
+    {
         question: "Does your work schedule permit a rigid time allocation",
         response: ["Yes", "No"]
     },
-    questionSix: {
+    {
         question: "What background or skills do you have in I.T? Select all that apply.",
         response: ["Microsoft Office", "Design Suites Creative (Adobe, etc)", "Programming", "Digital marketing",
                     "Data Science", "Hardware", "Cyber Security"]
     },
-    questionSeven: {
+    {
         question: "How many years of experience do you have in IT?",
         response: ["1-5", "6-10", "10+"]
     },
-    questionEight: {
+    {
         question: "Do you have any relevant certifications?",
         response: ["Yes", "No"]
-    }
-};
+    },
+];
 
-function onSelection() {
-    var chosenBtn = document.getElementById("question1");
-    var newColor = chosenBtn.getElementsByTagName("button");
-    for (var i = 0; i < newColor.length; i++) {
-        newColor[i].style.backgroundColor = "white";
-        newColor[i].style.Color = "red";
+// This creates the questions within our questionnaire
+var durrell_questions = document.getElementsByClassName('durrell_questions');
+for (let index = 0; index < durrell_questions.length; index++) {
+    var element = durrell_questions[index];
+    // This line inserts our questions into the html file
+    element.innerHTML = questions[index].question;
+    element.style.visibility = "hidden";
+    //for (var i = 0; i < element.length; i++) {
+}
+
+var choices = document.getElementsByClassName('choices');
+for (let index = 0; index < questions.length; index++) {
+    var question = questions[index],
+    response = question.response,
+    content = '';
+    for (let i = 0; i < response.length; i++) {
+        const r = response[i];
+        choices[index].innerHTML += '<button class="choice1">' + r + '</button>';
     }
 }
+
+function onSelection() {
+    var oneQuestion = document.getElementsByClassName("choices");
+    for (var i = 0; i < oneQuestion.length; i++) {
+        if (oneQuestion[i].checked) {
+            durrell_questions.innerHTML = question[0].questions;
+        }
+    }
+    alert("Select response!")
+    return false;
+}
+
+choices.onclick = onSelection;
